ed187139dadca3f5a67f2f0e9a4013710b942022
jbethune <jonathan.bethune@toptal.com>
2026-03-22 14:43:11 +0900

    Added health check route

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+
 class HelloWorldHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
@@ -6,6 +7,13 @@ class HelloWorldHandler(SimpleHTTPRequestHandler):
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(b'hello world')
+
+        elif self.path == '/health':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(b'{"status":"ok"}')
+
         else:
             super().do_GET()
 
